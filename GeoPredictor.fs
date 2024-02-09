86c4856b68e947b4a43483071705042b9b38d8f2
McMuttons <mcmuttons@gmail.com>
2024-02-09 23:41:08 +0100

    Fixed a display issue due to unhandled event.

--- GeoPredictor.fs
+++ GeoPredictor.fs
@@ -12,40 +12,34 @@ type GeoGrid (body, count, volcanism, temp) =
     member val Volcanism = volcanism
     member val Temp = temp
 
-    new() = GeoGrid("", 0, "", "")
+    new() = GeoGrid("", "", "", "")
 
 type Worker() =
     let mutable (Core:IObservatoryCore) = null
     let (GridCollection:ObservableCollection<obj>) = ObservableCollection<obj>()
     let mutable (UI:PluginUI) = null
 
-
     interface IObservatoryWorker with 
-        member this.StatusChange status = ()
-        member this.LogMonitorStateChanged args = ()
-
         member this.Load core = 
             Core <- core
             
             GridCollection.Add(GeoGrid())
             UI <- PluginUI(GridCollection)
-
-            Core.ClearGrid(this, UI)
                 
             ()
 
-        member this.get_Name () = "GeoPredictor"
-        member this.get_Version () = Assembly.GetCallingAssembly().GetName().Version.ToString()
-
-        member this.get_PluginUI () = UI
-
-        member this.get_Settings () = ()
-        member this.set_Settings settings = ()
-
         member this.JournalEvent event =
             match (event:JournalBase) with
-                | :? LoadGame as lg -> ()
                 | _ -> ()
+
+
+        member this.get_Name () = "GeoPredictor"
+        member this.get_Version () = Assembly.GetCallingAssembly().GetName().Version.ToString()
+        member this.get_PluginUI () = UI
+        member this.get_Settings () = ()
+        member this.set_Settings settings = ()
+        member this.StatusChange status = ()
+        member this.LogMonitorStateChanged args = ()
 
         // Deprecated, but needed for explicit interface
         member this.ReadAllStarted () = ()
