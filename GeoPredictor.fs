75287f26f72b0feb3d996bbabbbde85d458dd7c5
McMuttons <mcmuttons@gmail.com>
2024-02-18 15:52:49 +0100

    Change Dictionaries to native Maps, and make mutability more obvious.

--- GeoPredictor.fs
+++ GeoPredictor.fs
@@ -20,9 +20,9 @@ type Worker() =
     let mutable (Core:IObservatoryCore) = null
     let mutable (UI:PluginUI) = null
     let mutable (currentSystem:uint64) = 0UL
-    let GeoBodies = new Dictionary<string, BodyDetail>() 
-    let ScannedBodies = new Dictionary<string, ScannedBody>()
-    let GridCollection = new ObservableCollection<obj>()
+    let mutable GeoBodies = Map.empty 
+    let mutable ScannedBodies = Map.empty
+    let mutable GridCollection = ObservableCollection<obj>()
 
     // immutable
     let geoSignalType = "$SAA_SignalType_Geological;"
@@ -66,33 +66,33 @@ type Worker() =
             match (event:JournalBase) with 
                 | :? Scan as scan ->
                     if not (ScannedBodies.ContainsKey(scan.BodyName)) then
-                        ScannedBodies.Add (
+                        ScannedBodies <- ScannedBodies.Add (
                             scan.BodyName,
                             { Name = scan.BodyName; Materials = List.empty; Volcanism = scan.Volcanism; Temp = scan.SurfaceTemperature })
-                        updateGrid this (buildGridRows GeoBodies.Values ScannedBodies.Values)
+                        buildGridRows GeoBodies.Values ScannedBodies.Values |> updateGrid this
 
                 | :? SAASignalsFound as signalsFound ->  
                     if not (GeoBodies.ContainsKey(signalsFound.BodyName)) then  
                         signalsFound.Signals 
                         |> Seq.filter (fun s -> s.Type = geoSignalType)
                         |> Seq.iter (fun s ->
-                            GeoBodies.Add (
+                            GeoBodies <- GeoBodies.Add (
                                 signalsFound.BodyName,
                                 { Name = signalsFound.BodyName; 
                                   Count = s.Count; 
                                   GeosFound = Map.empty} ))
-                        updateGrid this (buildGridRows GeoBodies.Values ScannedBodies.Values)
+                        buildGridRows GeoBodies.Values ScannedBodies.Values |> updateGrid this
 
                 | :? FSDJump as jump ->
                     match ((jump :? CarrierJump) && (not (jump :?> CarrierJump).Docked)) with
                         | true -> ()
                         | false -> 
                             currentSystem <- setCurrentSystem currentSystem jump.SystemAddress
-                            updateGrid this (buildGridRows GeoBodies.Values ScannedBodies.Values)
+                            buildGridRows GeoBodies.Values ScannedBodies.Values |> updateGrid this
 
                 | :? Location as location ->
                     currentSystem <- setCurrentSystem currentSystem location.SystemAddress
-                    updateGrid this (buildGridRows GeoBodies.Values ScannedBodies.Values)
+                    buildGridRows GeoBodies.Values ScannedBodies.Values |> updateGrid this
 
                 | _ -> ()
 
@@ -100,7 +100,7 @@ type Worker() =
             if LogMonitorStateChangedEventArgs.IsBatchRead args.NewState then
                 Core.ClearGrid(this, buildHeaderRow)
             elif LogMonitorStateChangedEventArgs.IsBatchRead args.PreviousState then
-                updateGrid this (buildGridRows GeoBodies.Values ScannedBodies.Values)
+                buildGridRows GeoBodies.Values ScannedBodies.Values |> updateGrid this
 
         member this.get_Name () = "GeoPredictor"
         member this.get_Version () = version
