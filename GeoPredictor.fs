8a473509af682cdab7250fec7f9c05e49ac6ffeb
McMuttons <mcmuttons@gmail.com>
2024-02-19 14:54:09 +0100

    Flattened the geo bodies data structures for easier serialization and it now is fully functional as a reporter of which geos you've seen.

--- GeoPredictor.fs
+++ GeoPredictor.fs
@@ -6,12 +6,9 @@ open Observatory.Framework.Interfaces
 open System.Collections.ObjectModel
 open System.Reflection
 
-type GeoDetail = { Type:string; Scanned:bool }
-type BodyDetail = { Name:string; Count:int; GeosFound:Map<string, GeoDetail> }
-type Material = { Name:string; Percent:float32}
-type ScannedBody = { Name:string; Volcanism:string; Temp:float32 }
-type BodyId = { BodyName:string; SystemAddress:uint64; BodyId:int }
-
+type GeoDetail = { Type:string }
+type ScannedBody = { Name:string; Volcanism:string; Temp:float32; Count:int; GeosFound:List<GeoDetail> }
+type BodyId = { SystemAddress:uint64; BodyId:int }
 type GeoRow = { Body:string; Count:string; Type:string; Volcanism:string; Temp:string }
 
 type Worker() =
@@ -20,29 +17,107 @@ type Worker() =
     let mutable (Core:IObservatoryCore) = null
     let mutable (UI:PluginUI) = null
     let mutable currentSystem = 0UL
-    let mutable GeoBodies = Map.empty 
     let mutable ScannedBodies = Map.empty
     let mutable GridCollection = ObservableCollection<obj>()
 
     // immutable
     let geoSignalType = "$SAA_SignalType_Geological;"
+    let geoTypes = [
+        "$Codex_Ent_IceGeysers_WaterMagma_Name;";
+        "$Codex_Ent_IceFumarole_WaterMagma_Name;";
+        "$Codex_Ent_Geysers_WaterMagma_Name;";
+        "$Codex_Ent_Fumarole_WaterMagma_Name;";
+        "$Codex_Ent_Gas_Vents_WaterMagma_Name;";
+
+        "$Codex_Ent_IceGeysers_SulphurDioxideMagma_Name;";
+        "$Codex_Ent_IceFumarole_SulphurDioxideMagma_Name;";
+        "$Codex_Ent_Geysers_SulphurDioxideMagma_Name;";
+        "$Codex_Ent_Fumarole_SulphurDioxideMagma_Name;";
+        "$Codex_Ent_Gas_Vents_SulphurDioxideMagma_Name;";
+
+        "$Codex_Ent_IceGeysers_AmmoniaMagma_Name;";
+        "$Codex_Ent_IceFumarole_AmmoniaMagma_Name;";
+        "$Codex_Ent_Geysers_AmmoniaMagma_Name;";
+        "$Codex_Ent_Fumarole_AmmoniaMagma_Name;";
+        "$Codex_Ent_Gas_Vents_AmmoniaMagma_Name;";
+
+        "$Codex_Ent_IceGeysers_MethaneMagma_Name;";
+        "$Codex_Ent_IceFumarole_MethaneMagma_Name;";
+        "$Codex_Ent_Geysers_MethaneMagma_Name;";
+        "$Codex_Ent_Fumarole_MethaneMagma_Name;";
+        "$Codex_Ent_Gas_Vents_MethaneMagma_Name;";
+
+        "$Codex_Ent_IceGeysers_NitrogenMagma_Name;";
+        "$Codex_Ent_IceFumarole_NitrogenMagma_Name;";
+        "$Codex_Ent_Geysers_NitrogenMagma_Name;";
+        "$Codex_Ent_Fumarole_NitrogenMagma_Name;";
+        "$Codex_Ent_Gas_Vents_NitrogenMagma_Name;";
+
+        "$Codex_Ent_Lava_Spouts_SilicateMagma_Name;";
+        "$Codex_Ent_Lava_Spouts_IronMagma_Name;";
+
+        "$Codex_Ent_IceGeysers_WaterGeysers_Name;";
+        "$Codex_Ent_IceFumarole_WaterGeysers_Name;";
+        "$Codex_Ent_Geysers_WaterGeysers_Name;";
+        "$Codex_Ent_Fumarole_WaterGeysers_Name;";
+        "$Codex_Ent_Gas_Vents_WaterGeysers_Name;";
+ 
+        "$Codex_Ent_IceGeysers_CarbonDioxideGeysers_Name;";
+        "$Codex_Ent_IceFumarole_CarbonDioxideGeysers_Name;";
+        "$Codex_Ent_Geysers_CarbonDioxideGeysers_Name;";
+        "$Codex_Ent_Fumarole_CarbonDioxideGeysers_Name;";
+        "$Codex_Ent_Gas_Vents_CarbonDioxideGeysers_Name;";
+
+        "$Codex_Ent_IceGeysers_AmmoniaGeysers_Name;";
+        "$Codex_Ent_IceFumarole_AmmoniaGeysers_Name;";
+        "$Codex_Ent_Geysers_AmmoniaGeysers_Name;";
+        "$Codex_Ent_Fumarole_AmmoniaGeysers_Name;";
+        "$Codex_Ent_Gas_Vents_AmmoniaGeysers_Name;";
+
+        "$Codex_Ent_IceGeysers_MethaneGeysers_Name;";
+        "$Codex_Ent_IceFumarole_MethaneGeysers_Name;";
+        "$Codex_Ent_Geysers_MethaneGeysers_Name;";
+        "$Codex_Ent_Fumarole_MethaneGeysers_Name;";
+        "$Codex_Ent_Gas_Vents_MethaneGeysers_Name;";
+
+        "$Codex_Ent_IceGeysers_NitrogenGeysers_Name;";
+        "$Codex_Ent_IceFumarole_NitrogenGeysers_Name;";
+        "$Codex_Ent_Geysers_NitrogenGeysers_Name;";
+        "$Codex_Ent_Fumarole_NitrogenGeysers_Name;";
+        "$Codex_Ent_Gas_Vents_NitrogenGeysers_Name;";
+
+        "$Codex_Ent_IceGeysers_HeliumGeysers_Name;";
+        "$Codex_Ent_IceFumarole_HeliumGeysers_Name;";
+        "$Codex_Ent_Geysers_HeliumGeysers_Name;";
+        "$Codex_Ent_Fumarole_HeliumGeysers_Name;";
+        "$Codex_Ent_Gas_Vents_HeliumGeysers_Name;";
+
+        "$Codex_Ent_IceGeysers_SilicateVapourGeysers_Name;";
+        "$Codex_Ent_IceFumarole_SilicateVapourGeysers_Name;";
+        "$Codex_Ent_Geysers_SilicateVapourGeysers_Name;";
+        "$Codex_Ent_Fumarole_SilicateVapourGeysers_Name;";
+        "$Codex_Ent_Gas_Vents_SilicateVapourGeysers_Name;"]
+
     let version = Assembly.GetCallingAssembly().GetName().Version.ToString()         
    
-    let buildGridRow bodyId body (geoBodies:Map<BodyId,BodyDetail>) =
-        match geoBodies.TryFind bodyId with
-        | Some detail -> 
+    let buildGridEntry body =   
+        let firstRow = 
             { Body = body.Name;
-              Count = detail.Count.ToString();
-              Type = "Some fucken geology, I dunno";
-              Volcanism = body.Volcanism;
-              Temp = (floor body.Temp).ToString() + "K" }  
-        | None ->
-            { Body = "Failed to link body to details!"; Count = ""; Type = ""; Volcanism = ""; Temp = "" }                
+                Count = body.Count.ToString();
+                Type = "";
+                Volcanism = body.Volcanism;
+                Temp = (floor body.Temp).ToString() + "K" }
+        match body.GeosFound.IsEmpty with
+            | true -> [ firstRow ]
+            | false ->
+                [firstRow] |> List.append
+                    (body.GeosFound
+                        |> List.map (fun d -> { Body = ""; Count = ""; Type = d.Type; Volcanism = ""; Temp = ""}))                               
 
-    let buildGridRows currentSystem geoBodies scannedBodies =
+    let buildGridRows currentSystem scannedBodies =
         scannedBodies
         |> Map.filter (fun k _ -> k.SystemAddress = currentSystem)
-        |> Map.map (fun id body -> buildGridRow id body geoBodies)
+        |> Seq.collect (fun body -> buildGridEntry body.Value) 
 
     let buildNullRow = { Body = null; Count = null; Type = null; Volcanism = null; Temp = null }
     let buildHeaderRow = { Body = "GeoPredictor v" + version; Count = ""; Type = ""; Volcanism = ""; Temp = "" }
@@ -52,19 +127,42 @@ type Worker() =
             | true -> newSystem
             | false -> oldSystem
 
-    let updateGrid worker (core:IObservatoryCore) (gridRows:Map<_,_>) =
+    let updateGrid worker (core:IObservatoryCore) gridRows =
         match core.IsLogMonitorBatchReading with
             | true -> ()
             | false ->
                 core.ClearGrid(worker, buildNullRow)
                 core.AddGridItem(worker, buildHeaderRow)
-                core.AddGridItems(worker, Seq.cast(gridRows.Values))
+                core.AddGridItems(worker, Seq.cast(gridRows))
 
     let isNotNullOrEmpty string =
         match string with
             | null -> false
             | "" -> false
             | _ -> true
+
+    let addBodyDetails id name volcanism temp (bodies:Map<BodyId, ScannedBody>) =
+        match bodies |> Map.tryFind(id) with
+        | Some body ->
+            bodies.Add (id, { body with Name = name; Volcanism = volcanism; Temp = temp })
+        | None -> 
+            bodies.Add (id, { Name = name; Volcanism = volcanism; Temp = temp; Count = 0; GeosFound = List.empty })
+
+    let addGeoDetails id name count (bodies:Map<BodyId, ScannedBody>) =
+        match bodies |> Map.tryFind(id) with
+        | Some body ->
+            bodies.Add (id, { body with Count = count })
+        | None ->
+            bodies.Add (id, { Name = name; Volcanism = ""; Temp = 0f; Count = count; GeosFound = List.empty })
+
+    let addFoundDetails id geotype (bodies:Map<BodyId, ScannedBody>) =
+        match bodies |> Map.tryFind(id) with
+        | Some body ->
+            match body.GeosFound |> List.tryFind(fun g -> g.Type = geotype) with
+            | Some geo -> bodies
+            | None -> bodies.Add (id, { body with GeosFound = body.GeosFound |> List.append [{ Type = geotype }]})
+        | None ->
+            bodies.Add (id, { Name = ""; Volcanism = ""; Temp = 0f; Count = 0; GeosFound = [{ Type = geotype }]})
 
 
     interface IObservatoryWorker with 
@@ -79,30 +177,36 @@ type Worker() =
                 | :? Scan as scan ->
                     match scan.Landable && scan.Volcanism |> isNotNullOrEmpty with
                         | true -> 
-                            ScannedBodies <- ScannedBodies.Add(
-                                { BodyName = scan.BodyName; SystemAddress = currentSystem; BodyId = scan.BodyID },
-                                { Name = scan.BodyName; Volcanism = scan.Volcanism; Temp = scan.SurfaceTemperature })
-                            buildGridRows currentSystem GeoBodies ScannedBodies |> updateGrid this Core
+                            let id = { SystemAddress = scan.SystemAddress; BodyId = scan.BodyID }
+                            ScannedBodies <- addBodyDetails id scan.BodyName scan.Volcanism scan.SurfaceTemperature ScannedBodies
+                            buildGridRows currentSystem ScannedBodies |> updateGrid this Core
                         | false -> ()
 
-                | :? SAASignalsFound as signalsFound ->  
-                    signalsFound.Signals 
+                | :? SAASignalsFound as sigs ->  
+                    sigs.Signals 
                     |> Seq.filter (fun s -> s.Type = geoSignalType)
                     |> Seq.iter (fun s ->
-                        GeoBodies <- GeoBodies.Add (
-                            { BodyName = signalsFound.BodyName; SystemAddress = signalsFound.SystemAddress; BodyId = signalsFound.BodyID },
-                            { Name = signalsFound.BodyName; Count = s.Count; GeosFound = Map.empty} ))
-                    buildGridRows currentSystem GeoBodies ScannedBodies |> updateGrid this Core
+                        let id = { SystemAddress = sigs.SystemAddress; BodyId = sigs.BodyID }
+                        ScannedBodies <- addGeoDetails id sigs.BodyName s.Count ScannedBodies)
+                    buildGridRows currentSystem ScannedBodies |> updateGrid this Core
+
+                | :? CodexEntry as codexEntry ->
+                    let id = { BodyId = codexEntry.BodyID; SystemAddress = codexEntry.SystemAddress }
+                    match geoTypes |> List.tryFind (fun t -> t = codexEntry.Name) with
+                    | Some _ ->
+                        let id = { SystemAddress = codexEntry.SystemAddress; BodyId = codexEntry.BodyID }
+                        ScannedBodies <- addFoundDetails id codexEntry.Name_Localised ScannedBodies
+                        buildGridRows currentSystem ScannedBodies |> updateGrid this Core
+                    | None -> ()
 
                 | :? FSDJump as jump ->
                     if not ((jump :? CarrierJump) && (not (jump :?> CarrierJump).Docked)) then 
                         currentSystem <- setCurrentSystem currentSystem jump.SystemAddress
-                        buildGridRows currentSystem GeoBodies ScannedBodies |> updateGrid this Core
-                                
+                        buildGridRows currentSystem ScannedBodies |> updateGrid this Core               
 
                 | :? Location as location ->
                     currentSystem <- setCurrentSystem currentSystem location.SystemAddress
-                    buildGridRows currentSystem GeoBodies ScannedBodies |> updateGrid this Core
+                    buildGridRows currentSystem ScannedBodies |> updateGrid this Core
 
                 | _ -> ()
 
@@ -110,7 +214,7 @@ type Worker() =
             if LogMonitorStateChangedEventArgs.IsBatchRead args.NewState then
                 Core.ClearGrid(this, buildNullRow)
             elif LogMonitorStateChangedEventArgs.IsBatchRead args.PreviousState then
-                buildGridRows currentSystem GeoBodies ScannedBodies |> updateGrid this Core
+                buildGridRows currentSystem ScannedBodies |> updateGrid this Core
 
         member this.get_Name () = "GeoPredictor"
         member this.get_Version () = version
