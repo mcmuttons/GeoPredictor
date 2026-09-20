fef72a52f1b43a101e6309f7159053e5f833c3af
mcmuttons <668213+mcmuttons@users.noreply.github.com>
2026-09-20 13:44:43 +0200

    Filedeserialization was throwing exception instead of returning faultvaule.

--- GeoPredictor.fs
+++ GeoPredictor.fs
@@ -266,6 +266,7 @@ type Worker() =
                     { HasReadAllBeenRun = false; Version = settingsVersion; LastUpdateCheck = DateTime.MinValue }, Map.empty
                 | settings ->
                     settings, Serializer.fileDeserialize<Map<string,Set<CodexUnit>>> Map.empty (core.PluginStorageFolder + codexUnlocksFileName)
+
             let gridCollection = ObservableCollection<obj>()
             gridCollection.Add(GridBuilder.nullRow)
 
